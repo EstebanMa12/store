05ffb00397bacd8e0ce242d151da14e524faefda
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-15 16:01:34 -0500

    Add counter component to about page

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { CounterComponent } from '../../../shared/components/counter/counter.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CounterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
+import { log } from 'console';
+import { repeat } from 'rxjs';
 
 @Component({
   selector: 'app-counter',
@@ -8,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
+  @Input({required: true}) duration = 0;
+  @Input({required: true}) message='';
 
+
+  constructor(){
+    //NO ASYNC
+    // Before render
+    console.log('constructor');
+    console.log('-'.repeat(10));
+
+  }
+  ngOnChanges(changes: SimpleChanges){
+    // Before and during render
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+  }
 }
