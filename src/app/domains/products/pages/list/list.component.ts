5d6eba35b76e201f1949d807740dff025d9b92fd
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-16 10:21:17 -0500

    Add app-header component to list.component.html and list.component.ts

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
-
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
