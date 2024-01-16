1d7735dc5c9e0598feddb2cf9bdc04a85768b980
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-16 16:28:41 -0500

    Refactor import paths in list.component.ts

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,16 +1,16 @@
 import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
-import { Product } from '@shared/models/product.model';
-import { HeaderComponent } from '@shared/components/header/header.component';
-import { CartService } from '@shared/services/cart.service';
-import { ProductService } from '@shared/services/product.service';
+import { Product } from '../../../shared/models/product.model';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { CartService } from '../../../shared/services/cart.service';
+import { ProductService } from '../../../shared/services/product.service';
 @Component({
   selector: 'app-list',
   standalone: true,
   imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
   products = signal<Product[]>([]);
@@ -18,19 +18,16 @@ export class ListComponent {
   private cartService = inject(CartService);
   private productService = inject(ProductService);
 
-  ngOnInit(){
-    this.productService.getProducts()
-      .subscribe({
-        next: (products)=> {
-        this.products.set(products)
-        },
-        error: (err)=> console.error(err)
-      });
-    }
+  ngOnInit() {
+    this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products.set(products);
+      },
+      error: (err) => console.error(err),
+    });
+  }
   // Leyendo el output del hijo
-  addToCart(product:Product){
+  addToCart(product: Product) {
     this.cartService.addToCart(product);
   }
-
-
 }
