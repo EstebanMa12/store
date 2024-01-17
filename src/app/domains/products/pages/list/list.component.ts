f53200628f7af4c0d3f2912b9964cebcfe130cae
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-17 16:46:33 -0500

    Update list component to display products by category

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
@@ -7,10 +7,11 @@ import { CartService } from '../../../shared/services/cart.service';
 import { ProductService } from '../../../shared/services/product.service';
 import { CategoryService } from '../../../shared/services/category.service';
 import { Category } from '../../../shared/models/category.model';
+import { RouterLinkWithHref, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent, HeaderComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent, RouterModule, RouterLinkWithHref],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
@@ -22,16 +23,28 @@ export class ListComponent {
   private productService = inject(ProductService);
   private categoryService = inject(CategoryService);
 
+  @Input() category_id?: string;
+
   ngOnInit() {
     this.getProducts();
     this.getProductsByCategory();
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const category_id = changes['category_id']
+    if (category_id){
+      this.getProducts();
+    }
+
+  }
+
+
   // Leyendo el output del hijo
   addToCart(product: Product) {
     this.cartService.addToCart(product);
   }
   private getProducts(){
-    this.productService.getProducts().subscribe({
+    this.productService.getProducts(this.category_id).subscribe({
       next: (products) => {
         this.products.set(products);
       },
